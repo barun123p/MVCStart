82fe0a20bec15e50a6dbf739788b0446f3ca4631
barunp <barunp@gmail.com>
2018-08-29 20:18:15 -0400

    Changes

---
+++ MVCApp/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult ValidateUser(UserInfo userInfo)
+        {
+            bool isValidUser = UserData.GetUsers().Any(x => x.UserName == userInfo.UserName && x.Password == userInfo.Password);
+            if (isValidUser)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.ErrorMessage = "Access Denied";
+            return View("Login");
+
+
+
+        }
+    }
+}
---
+++ MVCApp/Models/UserData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class UserData
+    {
+        public static List<UserInfo> GetUsers()
+        {
+            List<UserInfo> userInfos = new List<UserInfo>();
+            for (int i=0;i<10; i++)
+            {
+                userInfos.Add(new UserInfo()
+                {
+                    Id = i,
+                    UserName = "t" + i.ToString(),
+                    Password = "p" + i.ToString()
+                });
+            }
+            return userInfos;
+        }
+    }
+}
